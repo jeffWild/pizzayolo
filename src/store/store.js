c5d50ab25fe98ec06dbe0eab3c6ad58b4fdf5a80
jeffWild <geoffrey_de_st_urs@hotmail.fr>
2019-08-29 16:01:11 +0200

    ajout du localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,28 @@
 import rootReducer from "./rootReducer";
 import {createStore} from "redux";
 
-const store = createStore(rootReducer);
+const loadState = () => {
+  const state = localStorage.getItem("pizzaYoloStore");
+  if (!state) {
+    return undefined;
+  }
+  return JSON.parse(state);
+};
+
+const saveState = state => {
+  const jsonState = JSON.stringify(state);
+  localStorage.setItem("pizzaYoloStore",jsonState);
+};
+
+const preloadedState = loadState();
+
+const store = createStore(
+  rootReducer,
+  preloadedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => {
+  saveState(store.getState());
+});  
 
 export default store;
